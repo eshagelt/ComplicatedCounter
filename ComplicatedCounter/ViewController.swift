4c08983ed69ab9fd19eab4693ce4678dd051003b
Anastasiya Kiryanova <squaschling@gmail.com>
2023-06-15 22:32:20 +0300

    И еще чуть подправила.

--- ComplicatedCounter/ViewController.swift
+++ ComplicatedCounter/ViewController.swift
@@ -9,40 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var counterText: UILabel!
+    @IBOutlet weak private var counterText: UILabel!
     
-    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak private var addButton: UIButton!
     
-    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak private var subtractButton: UIButton!
     
-    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak private var resetButton: UIButton!
     
-    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak private var historyTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-    }
-    
-    @IBAction func addButtonDidTap(_ sender: Any) {
-        text = "значение изменено на: +1"
-        counter += 1
-    }
-    
-    @IBAction func subtractButtonDidTap(_ sender: Any) {
-        if counter == 0 {
-            text = "попытка уменьшить значение счётчика ниже 0"
-            counter = 0
-        } else if counter > 0 {
-            text = "значение изменено на: -1"
-            counter -= 1
-        }
-    }
-    
-    @IBAction func resetButtonDidTap(_ sender: Any) {
-        text = "значение сброшено"
-        counter = 0
     }
     
     var counter: Int = 0 {
@@ -71,6 +49,26 @@ class ViewController: UIViewController {
     
     private func updateHistory(text: String) {
         historyTextView.text = historyTextView.text + "\n" + text
+    }
+    
+    @IBAction private func addButtonDidTap(_ sender: Any) {
+        text = "значение изменено на: +1"
+        counter += 1
+    }
+    
+    @IBAction private func subtractButtonDidTap(_ sender: Any) {
+        if counter == 0 {
+            text = "попытка уменьшить значение счётчика ниже 0"
+            counter = 0
+        } else if counter > 0 {
+            text = "значение изменено на: -1"
+            counter -= 1
+        }
+    }
+    
+    @IBAction private func resetButtonDidTap(_ sender: Any) {
+        text = "значение сброшено"
+        counter = 0
     }
     
     
